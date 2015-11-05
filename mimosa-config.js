7ad02a0e0c12a57dee1e029f424d1cd7c0d45a13
krishna rokhale <krishna.rokhale@gmail.com>
2015-11-05 23:34:35 +0530

    initial commit.

---
+++ mimosa-config.js
@@ -0,0 +1,18 @@
+exports.config = {
+  "modules": [
+    "copy",
+    "server",
+    "jshint",
+    "csslint",
+    "require",
+    "minify-js",
+    "minify-css",
+    "live-reload",
+    "bower",
+    "coffeescript",
+    "stylus",
+    "jade",
+    "client-jade-static",
+    "web-package"
+  ]
+}
